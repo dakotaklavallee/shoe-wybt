f6f9a078f9b1230bed2818383e8929362823ffa5
Dakota Lavallee <dakota.k.lavallee@gmail.com>
2022-05-01 17:56:57 -0500

    First Try

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './Components/Landings/HomePage';
+import LoginPage from './Components/Landings/LoginPage';
+import Navbar from './Components/Constants/Navbar';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:"100vh"}}>
+      <Navbar loggedIn={loggedIn} />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
