5bbfb251a50ecfd7893cdcc0522d7a9bc6fc1447
Dakota Lavallee <dakota.k.lavallee@gmail.com>
2022-06-29 15:53:13 -0500

    Sizing

--- src/Components/Avatars/AvatarSelect.tsx
+++ src/Components/Avatars/AvatarSelect.tsx
@@ -6,25 +6,25 @@ export default function AvatarSelect({ avatars, user }: any) {
     <Avatar key={tar.avatar_id} url={tar.avatar_url} id={tar.avatar_id} user={user} />
   ));
   return (
-    <div className="container d-flex align-items-center mt-3" style={{height: "90vh"}}>
+    <div className="container d-flex align-items-center" style={{height: "90vh"}}>
       <div className="col align-items-center">
         {avatars.length ? 
         <>
         <div className="row d-flex justify-content-center">
             <div className="text-center mr-3">{avatarMap[0]}</div>
-            <div className="text-center mr-3">{avatarMap[1]}</div>
+            <div className="text-center">{avatarMap[1]}</div>
         </div> 
         <div className="row d-flex justify-content-center mt-2">
             <div className="text-center mr-3">{avatarMap[2]}</div>
-            <div className="text-center mr-3">{avatarMap[3]}</div>
+            <div className="text-center">{avatarMap[3]}</div>
         </div> 
         <div className="row d-flex justify-content-center mt-2">
             <div className="text-center mr-3">{avatarMap[4]}</div>
-            <div className="text-center mr-3">{avatarMap[5]}</div>
+            <div className="text-center">{avatarMap[5]}</div>
         </div> 
         <div className="row d-flex justify-content-center mt-2">
             <div className="text-center mr-3">{avatarMap[6]}</div>
-            <div className="text-center mr-3">{avatarMap[7]}</div>
+            <div className="text-center">{avatarMap[7]}</div>
         </div> 
         </>
         :null}
